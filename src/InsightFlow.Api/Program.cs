b5fc9a8c2be18852c9253071f59ac549f62d3ded
Sepehr Frd <sepfrd@outlook.com>
2025-02-07 17:36:20 +0330

    feat: add basic projects and files

---
+++ src/InsightFlow.Api/Program.cs
@@ -0,0 +1,20 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/ping", () =>
+{
+    Results.Ok();
+})
+.WithName("Ping");
+
+app.Run();
